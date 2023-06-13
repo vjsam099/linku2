757221b300c34e7ef1cd15e3524a847617968c09
vjsam099 <40731454+vjsam099@users.noreply.github.com>
2023-06-13 19:05:50 +0700

    Add files via upload

---
+++ linku.js
@@ -0,0 +1,22 @@
+
+
+<!-- JS Safelink -->
+  <script src='https://cdn.statically.io/gh/vjsam099/linku/main/safelink.js' type='text/javascript'/>
+<script type='text/javascript'>
+protected_links = &quot;domain.com,blogspot.com,blogger.com,linkedin.com,facebook.com,twitter.com,google.com,feedburner.com&quot;;
+auto_safelink();
+</script>
+
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7065516430049331"
+     crossorigin="anonymous"></script>
+<!-- DTV -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7065516430049331"
+     data-ad-slot="8836116683"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
